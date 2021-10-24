d3c395a87a242ddd241aa4ee106a09c581d09243
lobt2000 <bkolodiy20013@gmail.com>
2021-10-24 11:57:24 +0300

    Add preloader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IsLoginGuard } from './guards/is-login.guard';
+import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './shared/components/reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'kindergarten', loadChildren: () => import('./layouts/layouts.module').then((m) => m.LayoutsModule), canActivate: [IsLoginGuard]
+  },
+  { path: 'login', component: LoginComponent, canLoad: [IsLoginGuard] },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  {
+    path: '',
+    redirectTo: 'kindergarten',
+    pathMatch: 'full'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
