d8d955e1533536b9baa7534b3c0fb19c04fe3ba1
lobt2000 <bkolodiy20013@gmail.com>
2021-10-24 11:55:36 +0300

    Init DB-Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDeU5N-h_3PqSxYGc7ARt9tzXsdnAT9fkA",
+    authDomain: "kindergarten-daed8.firebaseapp.com",
+    projectId: "kindergarten-daed8",
+    storageBucket: "kindergarten-daed8.appspot.com",
+    messagingSenderId: "180925751357",
+    appId: "1:180925751357:web:ae2802aa11481ba73fca54"
+  }
 };
 
 /*
